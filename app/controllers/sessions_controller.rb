2a94746e34b865a7c12474a95f87b73fb29f2651
rbn1993 <surfacerobin@outlook.com>
2018-11-05 23:47:21 +0100

    ADD: msg erros, succes, info... to login(welcome), modify simple form with placeholders etc

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user, success: 'Welcome to Organizer-2018!'
+    else
+      # Create an error message.
+      redirect_to :root, danger: 'Invalid email/password combination'
+
+    end
+  end
+
+  def destroy
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :email, presence: true, length: {maximum: 50 }
+  validates :password,  presence: true, length: { maximum: 12 }
+  has_secure_password
 end
