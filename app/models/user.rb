4cc3e00fdfc52f82d612af39a2c8da56cd4ca3a1
rbn1993 <surfacerobin@outlook.com>
2018-12-09 17:27:24 +0100

    Changes PC

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: {maximum: 4,message: "Max lo" }
   validates :email, presence: true, length: {maximum: 50 }
   validates :password,  presence: true, length: { maximum: 12 }
   has_secure_password
--- db/migrate/20180921212050_create_users.rb
+++ db/migrate/20180921212050_create_users.rb
@@ -1,10 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string :name
+      t.string :surname
       t.string :email
       t.string :password
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
